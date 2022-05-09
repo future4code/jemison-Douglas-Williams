1132b4d8615a7788175910f0cabeadb0658849a9
Douglas Williams <douglaswilliams298@gmail.com>
2022-05-08 21:43:52 -0300

    finalizando-projeto-blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,33 +1,36 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+      //Jogo BalckJack
 
+    console.log("Bem vindo(a) ao jogo Blackjack!")
 
+      let primeiraCartaUsuario = comprarCarta(); // Sorteia uma carta para o usuário.
+      let segundaCartaUsuario = comprarCarta(); // Sorteia outra carta para o usuário.
+      let primeiraCartaComputador = comprarCarta(); // Sorteia uma carta para o computador.
+      let segundaCartaComputador = comprarCarta(); // Sorteia outra carta para o computador.
 
-    console.log("Bem vindo ao jogo Blackjack!")
-
-    if(confirm("Quer iniciar uma nova rodada?")) {
+   if(confirm("Quer iniciar uma nova rodada?")) {
       // o que fazer se o usuário clicar "ok"
       console.log("Vamos pro jogo!")
+   
+      let pontuacaoUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+      let pontuacaoComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
 
-   } else {
-      // o que fazer se o usuário clicar "cancelar"
-      console.log("O joga acabou")
+      console.log(`Usuário - carta: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto} - ${pontuacaoUsuario}`) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+      console.log(`Computador - carta: ${primeiraCartaComputador.texto} ${segundaCartaComputador.texto} - ${pontuacaoComputador}`) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
 
+   if (pontuacaoUsuario > pontuacaoComputador) {
+      console.log(`Você ganhou!`)
+   } else if (pontuacaoUsuario < pontuacaoComputador){
+      console.log(`O computador ganhou!`)
+   } else if (pontuacaoUsuario == pontuacaoComputador){
+      console.log(`Empatou`)
    }
+   } else {
+   // o que fazer se o usuário clicar "cancelar"
+   console.log("O joga acabou")
+}
 
-   const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-   console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-   console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+
+   
+  
 
 
