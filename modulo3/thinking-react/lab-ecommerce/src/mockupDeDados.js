6ec55ea3caa2cdb201d75b173625ba255dc5d333
Douglas Williams <douglaswilliams298@gmail.com>
2022-07-14 23:27:47 -0300

    iniciando

---
+++ modulo3/thinking-react/lab-ecommerce/src/mockupDeDados.js
@@ -0,0 +1,43 @@
+export const Produtos= [
+    {
+        id: 1,
+        nomeProduto:"Produto 1",
+        valorProduto: 11,
+        img: 'https://picsum.photos/200/200?a=1'
+    },
+
+    {
+        id: 2,
+        nomeProduto:"Produto 2",
+        valorProduto: 22,
+        img: 'https://picsum.photos/200/200?a=1'
+        },
+
+    {
+        id: 3,
+        nomeProduto:"Produto 3",
+        valorProduto: 33,
+        img: 'https://picsum.photos/205/200?a=1'
+    },
+
+    {
+        id: 4,
+        nomeProduto:"Produto 4",
+        valorProduto: 44,
+        img: 'https://picsum.photos/210/200?a=1'
+    },
+
+    {
+        id: 5,
+        nomeProduto:"Produto 5",
+        valorProduto: 55,
+        img: 'https://picsum.photos/200/200?a=1'
+        },
+
+    {
+        id:6,
+        nomeProduto:"Produto 6",
+        valorProduto: 66,
+        img: 'https://picsum.photos/200/200?a=1'
+    },
+]
