8b4063c618aa00eb9963411a92322e783b7ce6c6
Douglas Williams <douglaswilliams298@gmail.com>
2022-10-11 22:13:12 -0300

    Fix: Respondi a pergunta da questão a

---
+++ modulo5/node-package-json/index.js
@@ -0,0 +1,6 @@
+import { data } from "./data.js";   // importei o arquivo que contem meu nome e idade;
+
+// Como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+// R: Criando uma variável que recebe os parâmetros passados na linha de comando utilizando o process.argv.
+
+
