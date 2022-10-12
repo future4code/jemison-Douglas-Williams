49ac2eb157510accab2389293bb8326df8f111c1
Douglas Williams <douglaswilliams298@gmail.com>
2022-10-11 21:36:19 -0300

    Fix: Se aprovado adiciona mais uma linha no exercício

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,31 @@
-console.log("exercicio 1");
+
+const returnNumber = (num) => {
+    if (typeof num === Number) {
+        return num
+    } else {
+        return `Erro. Parâmetro invalido(${num}`
+}
+
+
+
+
+const convertCelsius = (number, str) => {
+    const fahrenheitEmCelsius = number * 1.8 + 32
+    const kelvinEmCelsius = number + 273
+    const num = Number(number)
+
+    if (str === "F") {
+        return `${num}° Celsius é equivalente a ${fahrenheitEmCelsius}° Fahrenheit`
+    } else if (str === "K") {
+        return `${num}° Celsius é equivalente a ${kelvinEmCelsius}° Kelvin`
+    } else if (str ==! "F" || "K") {
+        return `Erro. Parâmetro invalido(${str})`
+    } else if ( typeof num === Number) {
+        return `Erro. Parâmetro invalido(${num})`
+    } else {
+        return `Erro`
+    }
+}
+
+
+console.log(convertCelsius("string", "F"));
