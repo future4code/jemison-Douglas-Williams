5a75a7f2ed9ec5b9ac192841cabb5fd4a51709d9
Douglas Williams <douglaswilliams298@gmail.com>
2022-10-15 17:49:26 -0300

    Feat: Se aplicado esse commit, inclui a resolução do exercicio 4

--- modulo5/intro-typescript/src/exercicio1.ts
+++ modulo5/intro-typescript/src/exercicio1.ts
@@ -1,4 +1,9 @@
 
+const a = process.argv[2]
+const b = process.argv[3]
+const c = process.argv[4]
+
+
 function checaTriangulo(a: Number, b: Number, c: Number) {
     if (a !== b && b !== c) {
       return "Escaleno";
@@ -10,8 +15,7 @@ function checaTriangulo(a: Number, b: Number, c: Number) {
   }
 
 
-
-
-
-
 console.log(checaTriangulo(1, 2, 3));
+console.log(checaTriangulo(1, 1, 1));
+console.log(checaTriangulo(2, 2, 3));
+
