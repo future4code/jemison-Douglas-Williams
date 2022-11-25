d19cf256f659b4d10c0f1e290dd369fb4cd4674d
Douglas Williams <douglaswilliams298@gmail.com>
2022-11-24 23:22:22 -0300

    Feat: Se aplicado esse commit, entrego o exercício

--- modulo5/servicos-backend/src/index.ts
+++ modulo5/servicos-backend/src/index.ts
@@ -1,13 +1,4 @@
-import express from "express"
+import app from "./app";
+import createUser from "./endpoints/createUser";
 
-import cors from 'cors'
-
-const app = express()
-
-app.use(express.json())
-
-app.use(cors())
-
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003");
-});
+app.post("/users/create", createUser);
