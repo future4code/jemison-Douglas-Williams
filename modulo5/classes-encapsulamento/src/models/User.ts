0d21c6eba1da9c73f7e6307a04ea79ab7d643d45
Douglas Williams <douglaswilliams298@gmail.com>
2022-12-06 21:09:19 -0300

    Feat: Se aplicado esse commitn dei inicio ao exercício e modifiquei os dois primeiros exercícios

---
+++ modulo5/classes-encapsulamento/src/models/User.ts
@@ -0,0 +1,13 @@
+export class User {
+    public id: string
+    public email: string
+    public password: string
+    constructor(id: string, email: string, password: string) {
+        this.id = id
+        this.email = email
+        this.password = password
+    }
+}
+
+// public, private, protected são modificadores de acesso
+
