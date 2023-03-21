df6182bfd7175de6843874d935868cfa64043f8d
Douglas Williams <douglaswilliams298@gmail.com>
2023-03-21 20:56:57 -0300

    Feat: Se aplicado esse commit, comecei o projeto instalando as dependencias e criei os arquivos.

---
+++ modulo7/resolvendo-problemas/src/exercise2/index.ts
@@ -0,0 +1,11 @@
+// Início
+
+// função que recebe uma string
+
+//     Serão feitas 3 operações com a string
+//         - Adicionar um caractere no final da string
+//         - Remover um caractere do final da string
+//         - Substituir um caractere por outro
+
+
+function 
